bb85dc6bde4db97261fb1bcf5349f07c55289486
Glenn Tarcea <glenn.tarcea@gmail.com>
2014-10-21 16:25:18 -0400

    Create services for storing users. Create test for the services.

---
+++ app/services/userdb.go
@@ -0,0 +1,40 @@
+package services
+
+import (
+	"fmt"
+
+	"github.com/gtarcea/1DevDayTalk2014/app"
+	"github.com/gtarcea/1DevDayTalk2014/schema"
+)
+
+type usersDB struct {
+	users map[string]*schema.User
+}
+
+func NewUsersDB() app.UserDBService {
+	return &usersDB{
+		users: make(map[string]*schema.User),
+	}
+}
+
+func (db *usersDB) GetByKey(key, value string) (schema.User, error) {
+	k := fmt.Sprintf("%s:%s", key, value)
+	user, ok := db.users[k]
+	if !ok {
+		return schema.User{}, app.ErrNotFound
+	}
+
+	return *user, nil
+}
+
+func (db *usersDB) Insert(email, fullname string) (schema.User, error) {
+	emailKey := fmt.Sprintf("email:%s", email)
+	fullnameKey := fmt.Sprintf("fullname:%s", fullname)
+	u := schema.User{
+		Email:    email,
+		Fullname: fullname,
+	}
+	db.users[emailKey] = &u
+	db.users[fullnameKey] = &u
+	return u, nil
+}
--- schema/user.go
+++ schema/user.go
@@ -1,7 +1,6 @@
 package schema
 
 type User struct {
-	ID       string `json:"id,omitempty"`
 	Email    string `json:"email"`
 	Fullname string `json:"fullname"`
 }
