729d467e6fab5ff8e833917a273f888572399b25
V. Glenn Tarcea <gtarcea@umich.edu>
2014-11-11 14:00:36 -0500

    Add detecting when we have disconnected from the server and clear out users. On connect query for new set of users.

--- website/app/users.js
+++ website/app/users.js
@@ -1,18 +1,30 @@
 App.Controllers.controller("usersController",
-                           ["$scope", "Restangular", "$timeout", usersController]);
-function usersController($scope, Restangular, $timeout) {
+                           ["$scope", "Restangular", "$timeout", "ws", usersController]);
+function usersController($scope, Restangular, $timeout, ws) {
+    var s = ws.get();
     $scope.users = [];
-    Restangular.one("api").all("users").getList().then(function(users) {
-        users.forEach(function(user) {
-            $scope.users.push({email: user.email, fullname: user.fullname});
+
+    function getUsers() {
+        Restangular.one("api").all("users").getList().then(function(users) {
+            users.forEach(function(user) {
+                $scope.users.push({email: user.email, fullname: user.fullname});
+            });
         });
-    });
+    }
 
     $scope.$on("newuser", function(event, user) {
         $timeout(function() {
             //console.log("add user %O", user);
             $scope.users.push(user);
         });
+    });
+
+    s.$on("$open", function() {
+        getUsers();
+    });
+
+    s.$on("$close", function() {
+        $scope.users = [];
     });
 }
 
