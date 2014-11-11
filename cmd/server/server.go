e16daca1a0c1d39063725b14d73f0e0a3e30e7ec
V. Glenn Tarcea <gtarcea@umich.edu>
2014-11-11 13:17:25 -0500

    Switch to new websocket interface. Delete some testing code.

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"time"
 
 	"code.google.com/p/go.net/websocket"
 
 	"github.com/gtarcea/1DevDayTalk2014/ws"
+	"github.com/gtarcea/1DevDayTalk2014/ws/events"
 	"github.com/jessevdk/go-flags"
 )
 
@@ -25,62 +25,22 @@ func main() {
 		os.Exit(1)
 	}
 
-	webserver(opts.Bind, opts.Port)
-	channelTest()
+	server(opts.Bind, opts.Port)
 }
 
-func webserver(bindTo string, port uint) {
-	//http.HandleFunc("/ws", serveWebsocket)
+func server(bindTo string, port uint) {
 	container := ws.NewRegisteredServicesContainer()
 	http.Handle("/api/", http.StripPrefix("/api", container))
+
 	webdir := os.Getenv("DEVDAY_WEBDIR")
 	dir := http.Dir(webdir)
 	http.Handle("/", http.FileServer(dir))
-	http.Handle("/ws", websocket.Handler(EchoServer))
+
+	hub := events.NewHub(events.NewEventConnections())
+	hub.Start()
+	s := events.NewServer(hub)
+	http.Handle("/ws", websocket.Handler(s.OnConnection))
+
 	addr := fmt.Sprintf("%s:%d", bindTo, port)
 	fmt.Println(http.ListenAndServe(addr, nil))
-}
-
-func EchoServer(ws *websocket.Conn) {
-	var msg string
-	websocket.Message.Receive(ws, &msg)
-	fmt.Println(msg)
-}
-
-type waiter struct {
-	newValue  string
-	observers chan bool
-}
-
-func channelTest() {
-	w := &waiter{
-		newValue:  "Hello",
-		observers: make(chan bool),
-	}
-
-	for i := 0; i < 5; i++ {
-		go startWaiter(w, i)
-	}
-
-	time.Sleep(5 * time.Second)
-	for i := 0; i < 5; i++ {
-		select {
-		case w.observers <- true:
-			fmt.Println("sent true")
-		case <-time.After(2 * time.Second):
-			fmt.Println("timeout")
-		}
-	}
-	time.Sleep(2 * time.Second)
-}
-
-func startWaiter(w *waiter, i int) {
-	fmt.Println("waiter", i, " waiting")
-	for {
-		select {
-		case <-w.observers:
-			fmt.Println("waiter", i, " got value", w.newValue)
-		}
-	}
-
 }
