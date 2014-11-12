83ad63403b1d4e3109e67c134ea70ab2a59d9754
Glenn Tarcea <glenn.tarcea@gmail.com>
2014-11-11 19:46:00 -0500

    Change connection status to also show Green or Red.

--- website/app/index.js
+++ website/app/index.js
@@ -4,17 +4,22 @@ App.Controllers.controller("indexController",
 
 function indexController($scope, ws) {
     $scope.model = {
-        connectionStatus: "Not Connected"
+        connectionStatus: "Not Connected",
+        connected: false
     };
 
     var s = ws.get();
 
     s.$on("$open", function() {
+        console.log("connected");
         $scope.model.connectionStatus = "Connected";
+        $scope.model.connected = true;
     });
 
     s.$on("$close", function() {
+        console.log("not connected");
         $scope.model.connectionStatus = "Not Connected";
+        $scope.model.connected = false;
     });
 
     s.$on("$error", function(e) {
