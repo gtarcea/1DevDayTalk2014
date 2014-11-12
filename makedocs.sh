9ad87fbb8d76e362646862636dad86cfd8f38c4d
V. Glenn Tarcea <gtarcea@umich.edu>
2014-11-12 15:22:31 -0500

    Fix up the makedocs script to reference the correct project.

--- makedocs.sh
+++ makedocs.sh
@@ -2,7 +2,7 @@
 
 for package in $(go list ./...)
 do
-    DIR=$(echo $package | sed 's%github.com/materials-commons/gohandy%.%')
+    DIR=$(echo $package | sed 's%github.com/gtarcea/1DevDayTalk2014%.%')
     mkdir -p docs/$DIR
     godoc $package > docs/$DIR/package.txt
 done
